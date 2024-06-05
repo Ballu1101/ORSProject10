537984b1995948bd36a92b72aae45ac9123ad439
CodeWithKalyani <kalyanisahu122000@gmail.com>
2024-06-05 15:39:03 +0530

    done

---
+++ projectORS/src/main/java/com/rays/common/message/MessageDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.common.message;
+
+import com.rays.common.BaseDAOInt;
+
+/**
+ * Role DAO interface.
+ * Kalyani sahu 
+ */
+public interface MessageDAOInt extends BaseDAOInt<MessageDTO> {
+
+}
---
+++ projectORS/src/main/java/com/rays/dao/CollegeDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.CollegeDTO;
+
+/**
+ * College DAO interface.
+ * Kalyani sahu 
+ */
+public interface CollegeDAOInt extends BaseDAOInt<CollegeDTO> {
+}
